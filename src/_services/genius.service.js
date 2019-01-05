ce10d7066e6d9c8a0fade70ac89acdfd6605ad24
Alex Gyujin Kim <alexkim205@yahoo.com>
2019-01-04 23:20:00 -0500

    Finished Genius API

---
+++ src/_services/genius.service.js
@@ -0,0 +1,136 @@
+// import * as apiKeys from "../_config/api-keys";
+// import https from "https";
+// import querystring from "querystring";
+const apiKeys = require("../_config/api-keys");
+const https = require("https");
+const querystring = require("querystring");
+
+const BASE_URL = "https://api.genius.com/";
+
+/*
+Genius API - https://docs.genius.com
+Only GET API exposed
+*/
+
+class Genius {
+  constructor() {
+    // Set the configuration settings
+    this.at = apiKeys.genius.access;
+    this.defaultTextFormat = { text_format: "dom" };
+  }
+
+  request(options, callback) {
+    return new Promise((resolve, reject) => {
+      // select http or https module, depending on reqested url
+      const { url, qs } = options;
+      const queries = querystring.stringify(qs);
+      const defaultOptions = {
+        path: `/${url}?${queries}`,
+        headers: {
+          Authorization: `Bearer ${this.at}`,
+          "Content-Type": "application/json"
+        }
+      };
+      const request = https.get(BASE_URL, defaultOptions, response => {
+        // handle http errors
+        if (response.statusCode !== 200) {
+          var payload = {
+            error: response.statusMessage,
+            status: response.statusCode
+          };
+          reject(payload);
+        }
+        // populate temporary data holder and parse at end
+        var body = "";
+        response.on("data", chunk => (body += chunk));
+        response.on("end", () => {
+          var parsed = JSON.parse(body);
+          resolve(parsed);
+        });
+      });
+      // handle connection errors of the request
+      request.on("error", err => reject(err));
+    });
+  }
+
+  getAnnotation(id, options) {
+    let request = {
+      url: `annotations/${id}`,
+      qs: { ...this.defaultTextFormat, ...options }
+    };
+    return this.request(request);
+  }
+
+  getReferents(id, options) {
+    let defaultOptions = {
+      // per_page: '',
+      // page: '',
+    };
+    let request = {
+      url: "referents",
+      qs: { ...this.defaultTextFormat, ...defaultOptions, song_id: id }
+    };
+    return this.request(request);
+  }
+
+  getSong(id, options) {
+    let request = {
+      url: `songs/${id}`,
+      qs: { ...this.defaultTextFormat, ...options }
+    };
+    return this.request(request);
+  }
+
+  getArtist(id, options) {
+    let defaultOptions = {
+      // per_page: '',
+      // page: '',
+    };
+    let request = {
+      url: `artists/${id}`,
+      qs: { ...this.defaultTextFormat, ...options }
+    };
+    return this.request(request);
+  }
+
+  getArtistSongs(id, options) {
+    let defaultOptions = {
+      sort: "popularity" // title (default) or popularity
+      // per_page: '',
+      // page: '',
+    };
+    let request = {
+      url: `artists/${id}/songs`,
+      qs: { ...this.defaultTextFormat, ...defaultOptions, ...options }
+    };
+    return this.request(request);
+  }
+
+  getWebPage(url, options) {
+    let request = {
+      url: "web_pages/lookup",
+      qs: { raw_annotatable_url: url }
+    };
+    return this.request(request);
+  }
+
+  searchSong(_q) {
+    let request = {
+      url: "search",
+      qs: { q: _q }
+    };
+    return this.request(request);
+  }
+}
+
+// var genius = new Genius();
+// genius
+//   .searchSong("HUMBLE")
+//   .then(data => {
+//     console.log(data.response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+module.exports = Genius;
---
+++ src/_tests/genius.spec.js
@@ -0,0 +1,57 @@
+const chai = require("chai");
+const Genius = require("../_services/genius.service");
+const expect = chai.expect;
+// import Genius from "../_services/genius.service";
+
+const sample = {
+  id: 2332455,
+  artistid: 2300,
+  url: "https://genius.com/Adele-hello-lyrics",
+  song: "Hello"
+};
+
+const genius = new Genius();
+
+const handleExpectations = data => {
+  expect(data.meta.status).to.equal(200);
+  expect(data).to.be.a("object");
+  expect(data).to.have.property("response");
+};
+
+describe("Genius API", () => {
+  describe("/GET annotation", () => {
+    it("it should get an annotation", () => {
+      return genius.getAnnotation(sample.id).then(handleExpectations);
+    });
+  });
+  describe("/GET referents", () => {
+    it("it should get referents for a song", () => {
+      return genius.getReferents(sample.id).then(handleExpectations);
+    });
+  });
+  describe("/GET song", () => {
+    it("it should get song", () => {
+      return genius.getSong(sample.id).then(handleExpectations);
+    });
+  });
+  describe("/GET artist", () => {
+    it("it should get an artist", () => {
+      return genius.getArtist(sample.artistid).then(handleExpectations);
+    });
+  });
+  describe("/GET artist songs", () => {
+    it("it should get an artist's discography", () => {
+      return genius.getArtistSongs(sample.artistid).then(handleExpectations);
+    });
+  });
+  describe("/GET webpage", () => {
+    it("it should get a webpage by url", () => {
+      return genius.getWebPage(sample.url).then(handleExpectations);
+    });
+  });
+  describe("/GET song search", () => {
+    it("it should search for a song", () => {
+      return genius.searchSong(sample.song).then(handleExpectations);
+    });
+  });
+});
