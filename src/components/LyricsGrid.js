22ee6c4968a9d521850c111acaf964151ca8645a
Alex Gyujin Kim <alexkim205@yahoo.com>
2019-01-09 23:18:16 -0500

    added grouping to grid

--- src/components/LyricsGrid.js
+++ src/components/LyricsGrid.js
@@ -17,11 +17,17 @@ class LyricsGrid extends Component {
     };
     this.svgRef = React.createRef();
 
-    this._makeCorpus = this._makeCorpus.bind(this);
+    this.setAsyncState = this.setAsyncState.bind(this);
+    this.processData = this.processData.bind(this);
+    this._DFS = this._DFS.bind(this);
+    this.findIslands = this.findIslands.bind(this);
     this.drawGrid = this.drawGrid.bind(this);
   }
 
-  _makeCorpus() {
+  setAsyncState = (newState) =>
+    new Promise((resolve) => this.setState(newState, () => resolve()));
+
+  processData() {
     // https://datavizcatalogue.com/methods/gantt_chart.html
     // https://bost.ocks.org/mike/miserables/
     // unique words count
@@ -35,8 +41,8 @@ class LyricsGrid extends Component {
       .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '') // remove punctuation
       .toLowerCase() // all words to lc
       .trim() // trim beginning and end of string
-      .split(/\s+/); // split by whitespace
-    // .slice(0, 50);
+      .split(/\s+/) // split by whitespace
+      // .slice(0, 50);
 
     // count and put in dictionary
     var count = {};
@@ -83,12 +89,69 @@ class LyricsGrid extends Component {
         };
       }
     }
-    this.setState({ lyricsCorpus, matrix, count }, () => {
-      this.drawGrid();
-    });
+
+    return this.setAsyncState({ lyricsCorpus, matrix, count });
+  }
+
+  _DFS = (rootX, rootY, visited, group, groupIndex) => {
+    // DFS traverse to find all neighbors on island and mark visited
+    const { matrix } = this.state;
+    let toVisit = [matrix[rootX][rootY]],
+      currNode = null;
+
+    let rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1], // relative neighboring indices
+      colNbr = [-1, 0, 1, -1, 1, -1, 0, 1],
+      side = visited.length;
+
+    while (toVisit.length > 0) {
+      currNode = toVisit.pop();
+      if (currNode.r < 10 && currNode.c < 10) console.log(currNode)
+      visited[currNode.r][currNode.c] = 1; // mark visited
+
+      // Add neighboring nodes that are not empty
+      for (let i = 0; i < 8; i++) {
+        let x = currNode.r + rowNbr[i];
+        let y = currNode.c + colNbr[i];
+        if (x >= 0 && x < side && y >= 0 && y < side) {
+          // within boundaries
+          if (visited[x][y] === 0 && matrix[x][y].i !== 0) {
+            // not visited and not empty
+            // add group index to data
+            matrix[x][y].g = groupIndex++;
+            group.push(matrix[x][y]); // add node to island
+            toVisit.push(matrix[x][y]); // visit neighbors of this node too
+          }
+        }
+      }
+    }
+  };
+
+  findIslands() {
+    const { matrix } = this.state;
+    var groups = []; // all islands kept here
+    var groupIndex = 1;
+
+    // build empty matrix of visited nodes
+    var visited = [];
+    for (let _i = 0; _i < matrix.length; _i++) {
+      visited[_i] = new Array(matrix.length).fill(0);
+    }
+    console.log(visited)
+    for (let row = 0; row < matrix.length; row++) {
+      for (let col = row; col < matrix.length; col++) {
+        // Visit unvisited cells that aren't empty
+        if (visited[row][col] === 0 && matrix[row][col].i !== 0) {
+          var group = [matrix[row][col]];
+          this._DFS(row, col, visited, group); // go off and find an island!
+          groups.push(group); // island has been found and visited
+        }
+      }
+    }
+    console.log(groups);
   }
 
   drawGrid() {
+    console.log('draw grid called');
     const node = this.svgRef.current;
     const { matrix, lyricsCorpus, count } = this.state;
 
@@ -148,7 +211,10 @@ class LyricsGrid extends Component {
   }
 
   componentDidMount() {
-    this._makeCorpus(); // make lyrics corpus => array of words and render
+    this.processData() // process lyrics data into matrix
+      .then(this.findIslands)
+      .then(this.drawGrid) // draw grid
+      .catch((err) => console.error(err));
   }
 
   render() {
